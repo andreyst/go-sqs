426c301c08c6ca73ff4d7ced3b4bc9f2cd68f11b
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2019-02-14 07:12:46 +0300

    Remove extraneous spaces

--- load-test.py
+++ load-test.py
@@ -10,8 +10,8 @@ def work():
   headers={'Content-Type':'application/x-www-form-urlencoded'}
   data={'Action':'SendMessage','MessageBody':'test1','QueueUrl':'myqueue'}
   for i in range(10000):
-    requests.post("http://localhost:8080", data=data, headers=headers) 
-  finish = time.time() 
+    requests.post("http://localhost:8080", data=data, headers=headers)
+  finish = time.time()
   print(finish-start)
 
 pool = Pool(processes=NUM_PROCESSES)              # start 4 worker processes
